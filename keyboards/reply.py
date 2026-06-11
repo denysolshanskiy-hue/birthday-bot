b4d73b2cf4197fadcfde1c2d38c621ff9744560e
denysolshanskiy-hue <denysolshanskiy@gmail.com>
2026-06-11 16:03:20 +0300

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -8,7 +8,7 @@ admin_keyboard = ReplyKeyboardMarkup(
             keyboard=[
         [KeyboardButton(text="📊 Статистика"), KeyboardButton(text="🎉 Активні збори")],
         [KeyboardButton(text="✅ Оплати"), KeyboardButton(text="👥 Учасники")],
-        [KeyboardButton(text="❌ Не оплатили"), KeyboardButton(text="💳 Картки"), KeyboardButton(text="🚀 Тестовий збір")],
+        [KeyboardButton(text="❌ Не оплатили"), KeyboardButton(text="💳 Картки")],
             ],
                 resize_keyboard=True
             )
